5a086af66357f983917d03019bd49b616ff6ab35
Chris Laptop <cezanne199422@gmail.com>
2026-05-15 23:39:16 +0200

    chore: add tailwind asset and ignore sqlite data

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ enum Route {
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
     dioxus_cookie::init();
@@ -32,6 +33,7 @@ fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
         Router::<Route> {}
     }
 }
